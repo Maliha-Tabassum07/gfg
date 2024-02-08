b8879793ce833636372034f06af18fe558c393d6
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-02-08 18:29:13 +0600

    Minimum swap practice

--- gfg-codes/src/practice/random/MinimumSwapForBinaryArray.java
+++ gfg-codes/src/practice/random/MinimumSwapForBinaryArray.java
@@ -1,4 +1,29 @@
 package practice.random;
 
 public class MinimumSwapForBinaryArray {
+    public static void main(String[] args) {
+        int array[]={0, 1, 0, 1, 0};
+        MinimumSwapForBinaryArray minimumSwapForBinaryArray=new MinimumSwapForBinaryArray();
+        System.out.println(minimumSwapForBinaryArray.minimumSwap(array));
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i]);
+        }
+    }
+    private int minimumSwap(int arr[]){
+        int count=0;
+        for (int i=arr.length-1;i>0;i--){
+            if (arr[i]==0 && arr[i-1]==1){
+                arr[i]=1;
+                arr[i-1]=0;
+                count++;
+            }
+            if (i!=arr.length-1 && arr[i]==1 && arr[i+1]==0){
+                arr[i]=0;
+                arr[i+1]=1;
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
