ed21dcd9de8cc57ebfe2b9b9955d29efb9ed291e
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-02-12 12:25:29 +0600

    Basic print array

---
+++ gfg-codes/src/DSA/print_elements_of_array/Solution.java
@@ -0,0 +1,10 @@
+package DSA.print_elements_of_array;
+
+class Solution {
+    //Just print the space seperated array elements
+    void printArray(int arr[], int n) {
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
