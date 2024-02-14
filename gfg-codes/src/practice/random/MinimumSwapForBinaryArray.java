1a52fb7bc0292f4d23354cc460963a8610e62ca5
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-02-14 15:05:28 +0600

    Reverse Array

--- gfg-codes/src/practice/random/MinimumSwapForBinaryArray.java
+++ gfg-codes/src/practice/random/MinimumSwapForBinaryArray.java
@@ -10,6 +10,7 @@ public class MinimumSwapForBinaryArray {
         }
     }
     private int minimumSwap(int arr[]){
+
         int count=0;
         for (int i=arr.length-1;i>0;i--){
             if (arr[i]==0 && arr[i-1]==1){
