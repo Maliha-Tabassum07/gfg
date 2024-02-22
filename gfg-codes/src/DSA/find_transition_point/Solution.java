b9f4b16e16a9699aa27e1fcf5ae34c0e0a5c63fe
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-02-22 15:24:18 +0600

    tranisition point

---
+++ gfg-codes/src/DSA/find_transition_point/Solution.java
@@ -0,0 +1,12 @@
+package DSA.find_transition_point;
+
+class Solution {
+    int transitionPoint(int arr[], int n) {
+        for(int i=0;i<n;i++){
+            if(arr[i]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
