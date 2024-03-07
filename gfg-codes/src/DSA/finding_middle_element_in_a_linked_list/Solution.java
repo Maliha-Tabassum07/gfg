bc3fee7575e144bd8fd418e5b64284e70162fc2b
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-03-07 16:12:41 +0600

    finding middle element

---
+++ gfg-codes/src/DSA/finding_middle_element_in_a_linked_list/Solution.java
@@ -0,0 +1,29 @@
+package DSA.finding_middle_element_in_a_linked_list;
+
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+
+
+class Solution
+{
+    int getMiddle(Node head)
+    {
+        int totalIndex=0;
+        for(Node n=head;n!=null;n=n.next){
+            totalIndex++;
+        }
+        int middleIndex=(totalIndex/2);
+        totalIndex=0;
+        for(Node n=head;n!=null;n=n.next){
+            if(totalIndex==middleIndex){
+                return n.data;
+            }
+            totalIndex++;
+        }
+        return -1;
+    }
+}
+
