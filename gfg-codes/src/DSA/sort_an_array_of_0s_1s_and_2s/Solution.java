a2eed5d136d396311bd0c67cb7c5c699b3672715
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-03-21 15:50:56 +0600

    sort 012 array

---
+++ gfg-codes/src/DSA/sort_an_array_of_0s_1s_and_2s/Solution.java
@@ -0,0 +1,30 @@
+package DSA.sort_an_array_of_0s_1s_and_2s;
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int count0=0;
+        int count1=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                count0++;
+            }
+            else if(a[i]==1){
+                count1++;
+            }
+        }
+        count1=count0+count1;
+        for(int i=0;i<n;i++){
+            if(i<count0){
+                a[i]=0;
+            }
+            else if(i<count1){
+                a[i]=1;
+            }
+            else{
+                a[i]=2;
+            }
+        }
+    }
+}
