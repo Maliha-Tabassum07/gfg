ff583979ce07f2cef626861ba55f32078c5ff3d6
Maliha-Tabassum07 <maliha.tabassum@bjitacademy.com>
2024-04-29 14:43:06 +0600

    better approach

--- gfg-codes/src/DSA/finding_middle_element_in_a_linked_list/Solution.java
+++ gfg-codes/src/DSA/finding_middle_element_in_a_linked_list/Solution.java
@@ -11,19 +11,25 @@ class Solution
 {
     int getMiddle(Node head)
     {
-        int totalIndex=0;
-        for(Node n=head;n!=null;n=n.next){
-            totalIndex++;
+//        int totalIndex=0;
+//        for(Node n=head;n!=null;n=n.next){
+//            totalIndex++;
+//        }
+//        int middleIndex=(totalIndex/2);
+//        totalIndex=0;
+//        for(Node n=head;n!=null;n=n.next){
+//            if(totalIndex==middleIndex){
+//                return n.data;
+//            }
+//            totalIndex++;
+//        }
+//        return -1;
+        Node fast=head, slow=head;
+        while (fast !=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        int middleIndex=(totalIndex/2);
-        totalIndex=0;
-        for(Node n=head;n!=null;n=n.next){
-            if(totalIndex==middleIndex){
-                return n.data;
-            }
-            totalIndex++;
-        }
-        return -1;
+        return slow.data;
     }
 }
 
